8b715ceef41df6ad26cd4b9e4812149e84f469df
manulo <manulo@ukr.net>
2015-09-22 20:13:25 +0300

    laba9 reborn not done

--- perl/laba9.pl
+++ perl/laba9.pl
@@ -1,24 +1,63 @@
 #!/usr/bin/perl -w
 use strict;
 
-#open (my $fh, "<", "./laba9.html");
+if (!$ARGV[0]) {
+    print "Specify HTML file in input arg\n";
+    exit (1);
+}
 
 my $body;
-my $in_body;
+my $in_tag;
 
-while (<>) { chomp($_)&&($body.=$_) if /<body>/ ... /<\/body>/; }
+while (<>) { chomp($_)&&($body.=$_) if /<body[\d\D].*?>/ ... /<\/body>/; }
 
-#print $body."\n";
-
-#delete <body></body>
-$body = $1 if $body =~ /<body>(.*)<\/body>/;
+$body = $1 if $body =~ m/<body[\d\D].*?>(.*)<\/body>/; #delete <body></body>
 
 while ($body) {
     if ($body =~ m%<([A-Za-z1-9]+)>([\w\s\.]+)</\1>%i) { 
-#	print "--- $&  ---- $2  ----- \n";
-	$body = $`.$';
-	print " --- $2\n";
+	$body = $` . $';
+#	print " --- $2\n";
+	$in_tag .= $2." ";
     }
     else { $body = undef; }
 }
+
+for ( (split (" ",$in_tag)) ) {
+    print "$_\n"  if $_ =~ /(.)\1/g;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
